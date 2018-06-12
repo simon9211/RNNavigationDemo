da55c8c6f35b3e48cc4a2129f1e91df297a84bfa
xiwang wang <simon9211@126.com>
2018-06-12 14:52:54 +0800

    done well again

--- src/common/movieCell.js
+++ src/common/movieCell.js
@@ -10,76 +10,74 @@ import {
   Text,
   View,
   Image,
+  TouchableOpacity,
 } from 'react-native';
-
+import Star from '../star';
  export default class MovieCell extends React.Component {
     static defaultProps = {
-       item: null
+       item: null,
+       movies: 0
      };
-    // static propTypes = {
-    //     value: React.PropTypes.string.isRequired,
-    // };
 
     constructor(props) {
         super(props);
     };
 
     render() {
-        const {item} = props;
+        const {item ,movies} = this.props;
         return (
-          <View>
-              <TouchableOpacity
-                        style={[
-                            styles.hotList, item.key + 1 == movies.length && styles.lastList
-                        ]}
-                        onPress={() => navigate('Detail', {
-                            id: item.value.id,
-                            callback: (data) => {
-                                //this.setState({childState: data})
-                            }
-                        })}
-                    >
-                        <View style={{
-                            flex: 1
-                        }}>
-                            <Image 
-                                source={{
-                                    uri: item.value.images.large.replace('webp', 'png')
-                                    }}
-                                style={{
-                                    width: 80,
-                                    height: 100
-                                }}/>
+            <TouchableOpacity
+                    style={[
+                        styles.hotList, item.key + 1 == movies && styles.lastList
+                    ]}
+                    onPress={() => navigate('Detail', {
+                        id: item.value.id,
+                        callback: (data) => {
+                            //this.setState({childState: data})
+                        }
+                    })}
+                >
+                    <View style={{
+                        flex: 1
+                    }}>
+                        <Image 
+                            source={{
+                                uri: item.value.images.large.replace('webp', 'png')
+                                }}
+                            style={{
+                                width: 80,
+                                height: 100,
+                                // margin: auto,
+                            }}/>
+                    </View>
+                    <View style={{
+                        flex: 2,
+                        alignItems: 'flex-start'
+                    }}>
+                        <Text style={styles.title}>{item.value.title}</Text>
+                        <View style={{marginTop:3,marginBottom:3}}>
+                            <Star value={item.value.rating.stars}/>
                         </View>
+                        <Text style={styles.smallFont}>导演：{item.value.directors[0].name}</Text>
+                        <Text style={styles.smallFont}>主演：{item.value.casts.map((v) => v.name).join('/')}</Text>
+                        <Text style={{
+                            lineHeight: 20,
+                            fontSize: 13
+                        }}>{item.value.collect_count}人看过</Text>
                         <View style={{
-                            flex: 2,
-                            alignItems: 'flex-start'
-                        }}>
-                            <Text style={styles.title}>{item.value.title}</Text>
-                            <View style={{marginTop:3,marginBottom:3}}>
-                                <Star value={item.value.rating.stars}/>
-                            </View>
-                            <Text style={styles.smallFont}>导演：{item.value.directors[0].name}</Text>
-                            <Text style={styles.smallFont}>主演：{item.value.casts.map((v) => v.name).join('/')}</Text>
-                            <Text style={{
-                                lineHeight: 20,
-                                fontSize: 13
-                            }}>{item.value.collect_count}人看过</Text>
-                            <View style={{
-                                flex: 0
-                                }}>
-                                <TouchableOpacity 
-                                    onPress={() => alert('购票')} 
-                                    style={styles.pay}>
-                                        <Text style={{
-                                        color: '#FF4E65',
-                                        fontWeight: '900'
-                                        }}>购票</Text>
-                                </TouchableOpacity>
-                            </View>
+                            flex: 0
+                            }}>
+                            <TouchableOpacity 
+                                onPress={() => alert('购票')} 
+                                style={styles.pay}>
+                                    <Text style={{
+                                    color: '#FF4E65',
+                                    fontWeight: '900'
+                                    }}>购票</Text>
+                            </TouchableOpacity>
                         </View>
-                    </TouchableOpacity>
-          </View>
+                    </View>
+                </TouchableOpacity>
         ); 
     }
   }
@@ -92,5 +90,34 @@ import {
     },
     star:{
         marginRight: 2,
-    }
+    },
+    hotList: {
+        paddingLeft: 18,
+        paddingRight: 18,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#EFEFEF'
+      },
+      lastList: {
+        borderBottomWidth: 0
+      },
+      title: {
+        marginTop: 10,
+        fontWeight: '900',
+        fontSize: 15
+      },
+      pay: {
+        width: 50,
+        height: 25,
+        marginTop: 10,
+        marginLeft: 20,
+        marginBottom: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth:1,
+        borderColor:'#FF4E65',
+        borderRadius:5,
+
+      }
   })
--- src/hotList.js
+++ src/hotList.js
@@ -17,8 +17,7 @@ import {
   ActivityIndicator
 } from 'react-native';
 
-import Star from './star';
-
+import MovieCell from './common/movieCell';
 
  export default class HotList extends React.Component {
     constructor(props) {
@@ -55,7 +54,6 @@ import Star from './star';
                 i++;
             })
             this.setState({movies: arrList, ready: false, refreshing: false});
-            console.log(arrList)
         }).catch((error) => {
             alert(error.toString());
         });
@@ -79,59 +77,7 @@ import Star from './star';
                 refreshing={this.state.refreshing}
                 key={movies.key}
                 renderItem={({item}) => {
-                    return (
-                        <TouchableOpacity
-                        style={[
-                            styles.hotList, item.key + 1 == movies.length && styles.lastList
-                        ]}
-                        onPress={() => navigate('Detail', {
-                            id: item.value.id,
-                            callback: (data) => {
-                                this.setState({childState: data})
-                            }
-                        })}
-                    >
-                        <View style={{
-                            flex: 1
-                        }}>
-                            <Image 
-                                source={{
-                                    uri: item.value.images.large.replace('webp', 'png')
-                                    }}
-                                style={{
-                                    width: 80,
-                                    height: 100
-                                }}/>
-                        </View>
-                        <View style={{
-                            flex: 2,
-                            alignItems: 'flex-start'
-                        }}>
-                            <Text style={styles.title}>{item.value.title}</Text>
-                            <View style={{marginTop:3,marginBottom:3}}>
-                                <Star value={item.value.rating.stars}/>
-                            </View>
-                            <Text style={styles.smallFont}>导演：{item.value.directors[0].name}</Text>
-                            <Text style={styles.smallFont}>主演：{item.value.casts.map((v) => v.name).join('/')}</Text>
-                            <Text style={{
-                                lineHeight: 20,
-                                fontSize: 13
-                            }}>{item.value.collect_count}人看过</Text>
-                            <View style={{
-                                flex: 0
-                                }}>
-                                <TouchableOpacity 
-                                    onPress={() => alert('购票')} 
-                                    style={styles.pay}>
-                                        <Text style={{
-                                        color: '#FF4E65',
-                                        fontWeight: '900'
-                                        }}>购票</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                    )
+                    return <MovieCell item={item} moviesCount={movies.length}/>
                 }}/>}
           </View>
         ); 
@@ -151,31 +97,5 @@ const styles = StyleSheet.create({
         width: 12,
         height: 12,
         marginRight: 2
-      },
-      hotList: {
-        height: 130,
-        paddingLeft: 18,
-        paddingRight: 18,
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderBottomColor: '#EFEFEF'
-      },
-      lastList: {
-        borderBottomWidth: 0
-      },
-      title: {
-        fontWeight: '900',
-        fontSize: 15
-      },
-      pay: {
-        width: 50,
-        height: 25,
-        marginLeft: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderWidth:1,
-        borderColor:'#FF4E65',
-        borderRadius:5,
       }
 })
